3896572edcb85f9826511a4041041c6003ee9539
Dennis Scheiba <git@dennis-scheiba.com>
2025-12-23 23:38:43 +0100

    Improve completion message implementation (#56)
    * use raw osc message to calculate message size
    * change const casting behavior
    * refactor getCompletionMsg signature to pair return type

--- src/dyngen.cpp
+++ src/dyngen.cpp
@@ -400,21 +400,18 @@ void pluginCmdCallbackCleanup(World *world, void *rawCallbackData) {
 // consumes a completion msg from the msg stack
 // makes completionMsg either a nullptr (no message) or point it
 // to the buffer within the osc message.
-int getCompletionMsg(sc_msg_iter* args, char* &completionMsg) {
+std::pair<int, const char*> getCompletionMsg(sc_msg_iter* args) {
   auto const completionMsgSize = static_cast<int>(args->getbsize());
+  const char* completionMsg = nullptr;
   if (completionMsgSize > 0) {
-    // make readPos non const such that it can be passed to
-    // `fDoAsynchronousCommand` which requires non-const ptr
-    auto* readPos = const_cast<char *>(args->rdpos);
+    auto* readPos = args->rdpos;
     // point to the buf data of the completion msg - args->getb
     // would make a copy which we do not want since
     // `fDoAsynchronousCommand` already copies the buffer
     completionMsg = readPos + sizeof(int32_t);
     args->skipb();
-  } else {
-    completionMsg = nullptr;
   }
-  return completionMsgSize;
+  return {completionMsgSize, completionMsg};
 }
 
 // runs in stage  1 (RT thread)
@@ -439,14 +436,13 @@ void dyngenAddFileCallback(World* inWorld, void* inUserData, struct sc_msg_iter*
     RTFree(inWorld, newLibraryEntry);
     return;
   }
-  char* completionMsg;
-  auto completionMsgSize = getCompletionMsg(args, completionMsg);
+  auto [completionMsgSize, completionMsg] = getCompletionMsg(args);
 
   newLibraryEntry->oldCode = nullptr;
 
   ft->fDoAsynchronousCommand(
     inWorld, nullptr, nullptr, static_cast<void*>(newLibraryEntry),
-    loadFileToDynGenLibrary, swapCode,deleteOldCode, pluginCmdCallbackCleanup, completionMsgSize, completionMsg);
+    loadFileToDynGenLibrary, swapCode,deleteOldCode, pluginCmdCallbackCleanup, completionMsgSize, const_cast<char*>(completionMsg));
 }
 
 // like `dyngenAddFileCallback` but instead of a path we obtain the
@@ -468,14 +464,13 @@ void dyngenAddScriptCallback(World* inWorld, void* inUserData, struct sc_msg_ite
     RTFree(inWorld, newLibraryEntry);
     return;
   }
-  char* completionMsg;
-  auto completionMsgSize = getCompletionMsg(args, completionMsg);
+  auto [completionMsgSize, completionMsg] = getCompletionMsg(args);
 
   newLibraryEntry->oldCode = nullptr;
 
   ft->fDoAsynchronousCommand(
     inWorld, nullptr, nullptr, static_cast<void*>(newLibraryEntry),
-    loadScriptToDynGenLibrary, swapCode,deleteOldCode, pluginCmdCallbackCleanup, completionMsgSize, completionMsg);
+    loadScriptToDynGenLibrary, swapCode,deleteOldCode, pluginCmdCallbackCleanup, completionMsgSize, const_cast<char*>(completionMsg));
 }
 
 // ********************
