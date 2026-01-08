34ab8d415dfb073109284d0958e9323438325637
Dennis Scheiba <git@dennis-scheiba.com>
2026-01-08 23:30:56 +0100

    re-include library code

---
+++ src/library.cpp
@@ -0,0 +1,344 @@
+#include "library.h"
+
+#include <sstream>
+
+void Library::dyngenAddFileCallback(
+  World *inWorld,
+  void *inUserData,
+  sc_msg_iter *args,
+  void *replyAddr
+) {
+  auto newLibraryEntry = static_cast<NewDynGenLibraryEntry *>(
+      RTAlloc(inWorld, sizeof(NewDynGenLibraryEntry)));
+  if (!newLibraryEntry) {
+    Print("ERROR: Failed to allocate memory for DynGen library entry\n");
+    return;
+  }
+  newLibraryEntry->hash = args->geti();
+  if (const char *codePath = args->gets()) {
+    auto codePathLength = strlen(codePath) + 1;
+    newLibraryEntry->oscString =
+        static_cast<char *>(RTAlloc(inWorld, codePathLength));
+    if (!newLibraryEntry->oscString) {
+      Print("ERROR: Failed to allocate memory for DynGen code library\n");
+      RTFree(inWorld, newLibraryEntry);
+      return;
+    }
+    std::copy_n(codePath, codePathLength, newLibraryEntry->oscString);
+  } else {
+    Print("ERROR: Invalid dyngenfile message\n");
+    RTFree(inWorld, newLibraryEntry);
+    return;
+  }
+
+  newLibraryEntry->numParameters = args->geti();
+  newLibraryEntry->parameterNamesRT =
+      static_cast<char **>(RTAlloc(inWorld, newLibraryEntry->numParameters));
+  for (int i = 0; i < newLibraryEntry->numParameters; i++) {
+    if (const char *rawParam = args->gets()) {
+      auto paramLength = strlen(rawParam) + 1;
+
+      auto paramName = static_cast<char *>(RTAlloc(inWorld, paramLength));
+      if (!paramName) {
+        Print("ERROR: Failed to allocate memory for DynGen parameter names\n");
+        for (int j = 0; j < i; j++) {
+          RTFree(inWorld, newLibraryEntry->parameterNamesRT[j]);
+        }
+        RTFree(inWorld, newLibraryEntry->oscString);
+        RTFree(inWorld, newLibraryEntry);
+        return;
+      }
+      std::copy_n(rawParam, paramLength, paramName);
+      newLibraryEntry->parameterNamesRT[i] = paramName;
+    } else {
+      Print("ERROR: Invalid dyngenscript message of parameters\n");
+      for (int j = 0; j < i; j++) {
+        RTFree(inWorld, newLibraryEntry->parameterNamesRT[j]);
+      }
+      RTFree(inWorld, newLibraryEntry->oscString);
+      RTFree(inWorld, newLibraryEntry);
+      return;
+    }
+  }
+
+  auto [completionMsgSize, completionMsg] = getCompletionMsg(args);
+
+  newLibraryEntry->oldCode = nullptr;
+  newLibraryEntry->oldParameterNames = nullptr;
+  newLibraryEntry->numOldParameterNames = 0;
+
+  ft->fDoAsynchronousCommand(inWorld, nullptr, nullptr,
+                             static_cast<void *>(newLibraryEntry),
+                             loadFileToDynGenLibrary, swapCode, deleteOldCode,
+                             pluginCmdCallbackCleanup, completionMsgSize,
+                             const_cast<char *>(completionMsg));
+}
+
+void Library::addScriptCallback(
+  World *inWorld,
+  void *inUserData,
+  sc_msg_iter *args,
+  void *replyAddr
+) {
+  auto newLibraryEntry = static_cast<NewDynGenLibraryEntry *>(
+      RTAlloc(inWorld, sizeof(NewDynGenLibraryEntry)));
+  if (!newLibraryEntry) {
+    Print("ERROR: Failed to allocate memory for DynGen library entry\n");
+    return;
+  }
+  newLibraryEntry->hash = args->geti();
+  if (const char *oscCode = args->gets()) {
+    auto oscCodeLength = strlen(oscCode) + 1;
+    newLibraryEntry->oscString =
+        static_cast<char *>(RTAlloc(inWorld, oscCodeLength));
+    if (!newLibraryEntry->oscString) {
+      Print("ERROR: Failed to allocate memory for DynGen code library\n");
+      RTFree(inWorld, newLibraryEntry);
+      return;
+    }
+    std::copy_n(oscCode, oscCodeLength, newLibraryEntry->oscString);
+  } else {
+    Print("ERROR: Invalid dyngenscript message\n");
+    RTFree(inWorld, newLibraryEntry);
+    return;
+  }
+
+  newLibraryEntry->numParameters = args->geti();
+  newLibraryEntry->parameterNamesRT =
+      static_cast<char **>(RTAlloc(inWorld, newLibraryEntry->numParameters));
+  for (int i = 0; i < newLibraryEntry->numParameters; i++) {
+    if (const char *rawParam = args->gets()) {
+      auto paramLength = strlen(rawParam) + 1;
+
+      auto paramName = static_cast<char *>(RTAlloc(inWorld, paramLength));
+      if (!paramName) {
+        Print("ERROR: Failed to allocate memory for DynGen parameter names\n");
+        for (int j = 0; j < i; j++) {
+          RTFree(inWorld, newLibraryEntry->parameterNamesRT[j]);
+        }
+        RTFree(inWorld, newLibraryEntry->oscString);
+        RTFree(inWorld, newLibraryEntry);
+        return;
+      }
+      std::copy_n(rawParam, paramLength, paramName);
+      newLibraryEntry->parameterNamesRT[i] = paramName;
+    } else {
+      Print("ERROR: Invalid dyngenscript message of parameters\n");
+      for (int j = 0; j < i; j++) {
+        RTFree(inWorld, newLibraryEntry->parameterNamesRT[j]);
+      }
+      RTFree(inWorld, newLibraryEntry->oscString);
+      RTFree(inWorld, newLibraryEntry);
+      return;
+    }
+  }
+  auto [completionMsgSize, completionMsg] = getCompletionMsg(args);
+
+  newLibraryEntry->oldCode = nullptr;
+  newLibraryEntry->oldParameterNames = nullptr;
+  newLibraryEntry->numOldParameterNames = 0;
+
+  ft->fDoAsynchronousCommand(inWorld, nullptr, nullptr,
+                             static_cast<void *>(newLibraryEntry),
+                             loadScriptToDynGenLibrary, swapCode, deleteOldCode,
+                             pluginCmdCallbackCleanup, completionMsgSize,
+                             const_cast<char *>(completionMsg));
+}
+
+bool Library::loadScriptToDynGenLibrary(World *world,
+                                               void *rawCallbackData) {
+  const auto entry = static_cast<NewDynGenLibraryEntry*>(rawCallbackData);
+  const auto codeLength = strlen(entry->oscString) + 1;
+  auto *codeBuffer = new char[codeLength];
+  std::copy_n(entry->oscString, codeLength, codeBuffer);
+  entry->code = codeBuffer;
+
+  entry->parameterNamesNRT = new char *[entry->numParameters];
+  for (int i = 0; i < entry->numParameters; i++) {
+    auto paramLength = strlen(entry->parameterNamesRT[i]) + 1;
+    auto *paramBuffer = new char[paramLength];
+    std::copy_n(entry->parameterNamesRT[i], paramLength, paramBuffer);
+    entry->parameterNamesNRT[i] = paramBuffer;
+  }
+  return true;
+}
+
+bool Library::loadFileToDynGenLibrary(World *world,
+                                             void *rawCallbackData) {
+  auto entry = static_cast<NewDynGenLibraryEntry *>(rawCallbackData);
+
+  auto codeFile = std::ifstream(entry->oscString, std::ios::binary);
+  if (!codeFile.is_open()) {
+    Print("ERROR: Could not open DynGen file at %s\n", entry->oscString);
+    return false;
+  }
+
+  std::stringstream codeStream;
+
+  codeFile.seekg(0, std::ios::end);
+  const std::streamsize codeSize = codeFile.tellg();
+  codeFile.seekg(0);
+
+  // add /0
+  auto *codeBuffer = new char[codeSize + 1];
+  codeFile.read(codeBuffer, codeSize);
+  codeBuffer[codeSize] = '\0';
+
+  entry->code = codeBuffer;
+
+  entry->parameterNamesNRT = new char *[entry->numParameters];
+  for (int i = 0; i < entry->numParameters; i++) {
+    auto paramLength = strlen(entry->parameterNamesRT[i]) + 1;
+    auto *paramBuffer = new char[paramLength];
+    std::copy_n(entry->parameterNamesRT[i], paramLength, paramBuffer);
+    entry->parameterNamesNRT[i] = paramBuffer;
+  }
+
+  // continue to next stage
+  return true;
+}
+
+bool Library::swapCode(World *world, void *rawCallbackData) {
+  const auto entry = static_cast<NewDynGenLibraryEntry *>(rawCallbackData);
+
+  CodeLibrary *node = gLibrary;
+  while (node && node->id != entry->hash) {
+    node = node->next;
+  }
+
+  if (!node) {
+    auto *newNode = static_cast<CodeLibrary *>(RTAlloc(world, sizeof(CodeLibrary)));
+    if (!newNode) {
+      Print("ERROR: Failed to allocate memory for new code library\n");
+      return true;
+    }
+    newNode->next = gLibrary;
+    newNode->id = entry->hash;
+    newNode->dynGen = nullptr;
+    newNode->code = entry->code;
+    newNode->numParameters = entry->numParameters;
+    newNode->parameters = entry->parameterNamesNRT;
+    gLibrary = newNode;
+  } else {
+    // swap code
+    entry->oldCode = node->code;
+    entry->numOldParameterNames = node->numParameters;
+    entry->oldParameterNames = node->parameters;
+
+    node->code = entry->code;
+    node->numParameters = entry->numParameters;
+    node->parameters = entry->parameterNamesNRT;
+
+    auto dynGen = node->dynGen;
+    while (dynGen != nullptr) {
+      // although the code can be updated, the referenced code
+      // lives long enough b/c in worst case there is already
+      // a new code in the pipeline at stage2 where the old code
+      // would be destroyed in its stage4.
+      // Yet we only need to access the code in stage 2 in our callback,
+      // where it could not have been destroyed yet.
+      // See
+      // https://github.com/capital-G/DynGen/pull/40#discussion_r2599579920
+      // clang-format off
+/*
+     ┌─────────┐             ┌──────────┐           ┌─────────┐          ┌──────────┐
+     │STAGE1_RT│             │STAGE2_NRT│           │STAGE3_RT│          │STAGE4_NRT│
+     └────┬────┘             └─────┬────┘           └────┬────┘          └─────┬────┘
+          │loadFileToDynGenLibrary │                     │                     │
+          │───────────────────────>│                     │                     │
+          │                        │                     │                     │
+          │                        │      swapCode       │                     │
+          │                        │────────────────────>│                     │
+          │                        │                     │                     │
+          │loadFileToDynGenLibrary │                     │                     │
+          │ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ >│                     │                     │
+          │                        │                     │                     │
+          │     ╔════════════════╗ │createVmAndCompileA  │                     │
+          │     ║accessing code ░║ │<────────────────────│                     │
+          │     ╚════════════════╝ │                     │                     │
+          │                        │createVmAndCompileB  │                     │
+          │                        │<────────────────────│                     │
+          │                        │                     │                     │
+          │    ╔═════════════════╗ │      swapCode       │                     │
+          │    ║code -> oldCode ░║ │ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─>│                     │
+          │    ╚═════════════════╝ │                     │                     │
+          │                        │                     │   deleteOldCode     │
+          │                        │                     │────────────────────>│
+          │                        │                     │                     │
+          │                        │  swapVmPointersA    │                     │
+          │                        │────────────────────>│                     │
+          │                        │                     │                     │
+          │                        │  swapVmPointersB    │                     │
+          │                        │────────────────────>│                     │
+          │                        │                     │                     │
+          │                 ╔══════╧═══════════════════╗ │   deleteOldCode     │
+          │                 ║deleting code as oldCode ░║ │ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─>│
+          │                 ╚══════╤═══════════════════╝ │                     │
+          │                        │                     │    deleteOldVm      │
+          │                        │                     │────────────────────>│
+     ┌────┴────┐             ┌─────┴────┐           ┌────┴────┐          ┌─────┴────┐
+     │STAGE1_RT│             │STAGE2_NRT│           │STAGE3_RT│          │STAGE4_NRT│
+     └─────────┘             └──────────┘           └─────────┘          └──────────┘
+
+source code:
+```plantuml
+@startuml
+STAGE1_RT -> STAGE2_NRT : loadFileToDynGenLibrary
+STAGE2_NRT -> STAGE3_RT : swapCode
+STAGE1_RT --> STAGE2_NRT : loadFileToDynGenLibrary
+STAGE3_RT -> STAGE2_NRT : createVmAndCompileA
+note left: accessing code
+STAGE3_RT -> STAGE2_NRT : createVmAndCompileB
+STAGE2_NRT --> STAGE3_RT: swapCode
+note left: code -> oldCode
+STAGE3_RT -> STAGE4_NRT : deleteOldCode
+STAGE2_NRT -> STAGE3_RT: swapVmPointersA
+STAGE2_NRT -> STAGE3_RT: swapVmPointersB
+STAGE3_RT --> STAGE4_NRT : deleteOldCode
+note left: deleting code as oldCode
+STAGE3_RT -> STAGE4_NRT : deleteOldVm
+@enduml
+```
+*/
+      // clang-format on
+      dynGen->updateCode(entry->code, entry->parameterNamesNRT);
+      dynGen = dynGen->mNextDynGen;
+    }
+  }
+  return true;
+}
+
+bool Library::deleteOldCode(World *world, void *rawCallbackData) {
+  auto entry = static_cast<NewDynGenLibraryEntry *>(rawCallbackData);
+  delete[] entry->oldCode;
+  for (int i = 0; i < entry->numOldParameterNames; i++) {
+    delete[] entry->oldParameterNames[i];
+  }
+  delete[] entry->oldParameterNames;
+  return true;
+}
+
+void Library::pluginCmdCallbackCleanup(World *world,
+                                              void *rawCallbackData) {
+  auto callBackData = static_cast<NewDynGenLibraryEntry *>(rawCallbackData);
+  for (int i = 0; i < callBackData->numParameters; i++) {
+    RTFree(world, callBackData->parameterNamesRT[i]);
+  }
+  RTFree(world, callBackData->parameterNamesRT);
+  RTFree(world, callBackData->oscString);
+  RTFree(world, callBackData);
+}
+
+std::pair<int, const char *> Library::getCompletionMsg(sc_msg_iter *args) {
+  auto const completionMsgSize = static_cast<int>(args->getbsize());
+  const char *completionMsg = nullptr;
+  if (completionMsgSize > 0) {
+    auto *readPos = args->rdpos;
+    // point to the buf data of the completion msg - args->getb
+    // would make a copy which we do not want since
+    // `fDoAsynchronousCommand` already copies the buffer
+    completionMsg = readPos + sizeof(int32_t);
+    args->skipb();
+  }
+  return {completionMsgSize, completionMsg};
+}
---
+++ src/library.h
@@ -0,0 +1,186 @@
+#pragma once
+#include "dyngen.h"
+
+#include <fstream>
+#include <SC_PlugIn.hpp>
+
+// forward declarations
+class DynGen;
+struct CodeLibrary;
+struct DynGenStub;
+struct DynGenCallbackData;
+
+extern InterfaceTable *ft;
+extern CodeLibrary *gLibrary;
+
+/*! @brief Wraps a DynGen with a ref counter.
+ *  RT owned
+ *
+ *  @discussion because we use async command to delay the vm initialization
+ *  of a DynGen it is possible that the server deletes the UGen/DynGen while
+ *  we are preparing the vm.
+ *  In case we would put the vm in place, this would result in a crash
+ *  because the DynGen object has already been deleted by the server.
+ *  Therefore, instead of passing around the DynGen itself we pass around
+ *  this DynGenStub, which also holds a reference counter.
+ *  The ref count starts at 1 and gets incremented during each callback.
+ *  When a DynGen gets destroyed, the ref counter gets decremented and its
+ *  DynGen reference gets set to nullptr.
+ *  When an async command wants to swap the vm,  a nullptr check
+ *  can be performed on mObject, indicating if the object is still
+ *  existing or not.
+ *  Once the reference counter hits 0, the stub can be destroyed, which can
+ *  either happen during ~DynGen or within the free function of the async
+ *  update command.
+ */
+struct DynGenStub {
+  DynGen* mObject;
+  size_t mRefCount;
+};
+
+/*! @brief Stores code and associated Dyngen instances
+ *  RT managed
+ *
+ *  @discussion A linked list which manages stores the code under a given ID
+ *  (id/code) and also another linked list (DynGen*) which stores all the running
+ *  DynGen instances with the associated code, which allows us to update
+ *  the running instances in case the code changes.
+ *  There is currently no way to delete items from the linked list.
+ */
+struct CodeLibrary {
+  /*! @brief the next entry in the linked list */
+  CodeLibrary *next;
+  /*! @brief we refer to scripts via ID in order to avoid storing
+   *  and sending strings via OSC
+   */
+  int id;
+  /*! @brief  references the first DynGen - all other instances can be accessed
+   *  through the double linked list of DynGen
+   */
+  DynGen *dynGen;
+  /*! @brief the eel2 code currently associated with the DynGen instance */
+  char* code;
+  /*! @brief parameters which need to be exposed by - referenced by the integer
+   *  position within the array
+   */
+  char** parameters;
+  int numParameters;
+};
+
+/*! @brief A struct to be passed around to update already running dyngen nodes
+ */
+struct DynGenCallbackData {
+  /*! @brief vm is NRT managed - we flip the vm in RT thread, but perpare and
+   *  delete it in NRT context
+   */
+  EEL2Adapter *vm;
+  EEL2Adapter *oldVm;
+
+  /*! @brief the running dyngen stub to be updated */
+  DynGenStub *dynGenStub;
+  /*! @brief the new code to be used */
+  const char* code;
+  char** parameters;
+
+  /*! @brief vm init */
+  uint32 numInputChannels;
+  uint32 numOutputChannels;
+  uint32 numParameters;
+
+  int sampleRate;
+  int blockSize;
+  /*! @brief necessary to access params such as sample rate and RTFree */
+  World *world;
+  /*! @brief necessary for accessing local buffers */
+  Graph *parent;
+};
+
+/*! @brief The callback payload to enter a new entry into the code library,
+ *  which gets invoked via an OSC message/command.
+ */
+struct NewDynGenLibraryEntry {
+  /*! @brief while in sclang land we use strings to identify */
+  int hash;
+
+  /*! @discussion This can hold 2 alternatives, both RT managed and 0 terminated
+   *  Alternative A: read code from file
+   *  absolute path to the file storing the DynGen code
+   *  Alternative B: code was bundled within OSC message
+   *  RT managed code we receive via script command
+   */
+  char* oscString;
+
+  /*! @discussion when registering a new script we also need to know which
+   *  parameter names we need to expose via the script.
+   *  We therefore store an array which matches the position of each
+   *  arg from the client so the server only needs to transfer
+   *  the array position in its signal instead of transferring
+   *  a script
+   */
+  char** parameterNamesRT;
+  int numParameters;
+
+  /*! @brief the newly received code - NRT managed */
+  char* code;
+  /*! @brief the parameters - NRT version */
+  char** parameterNamesNRT;
+
+  /*! @brief the code to be replaced and should be deleted - NRT managed */
+  char* oldCode;
+  int numOldParameterNames;
+  char** oldParameterNames;
+};
+
+
+class Library {
+public:
+  /*! @brief runs in stage  1 (RT thread)
+   *  responds to an osc message on the RT thread - we therefore have to
+   *  copy the OSC data to a new struct which then gets passed to another
+   *  callback which runs in stage 2 (non-RT thread).
+   *  We have to free the created struct afterward via
+   *  `pluginCmdCallbackCleanup`.
+   */
+  static void dyngenAddFileCallback(World *inWorld, void *inUserData,
+                                    struct sc_msg_iter *args, void *replyAddr);
+
+  /*! @brief like `dyngenAddFileCallback` but instead of a path we obtain the
+   *  script within the OSC message.
+   */
+  static void addScriptCallback(World *inWorld, void *inUserData,
+                                struct sc_msg_iter *args, void *replyAddr);
+
+private:
+  /*! @brief this runs in stage 2 (NRT) and copies the content of the
+   *  RT owned code to a NRT owned code
+   */
+  static bool loadScriptToDynGenLibrary(World *world, void *rawCallbackData);
+
+  /*! @brief this runs in stage 2 (NRT) and loads the content of the file
+   *  which gets passed to stage 3 (RT)
+   */
+  static bool loadFileToDynGenLibrary(World *world, void *rawCallbackData);
+
+  /*! @brief runs in stage 3 (RT-thread)
+   *
+   *  @discussion The code string gets entered into the library
+   *  by traversing it as a linked list.
+   *  If the hash ID already exists the code gets updated and all running
+   *  instances should be updated.
+   */
+  static bool swapCode(World *world, void *rawCallbackData);
+
+  /*! @brief runs in stage 4 (non-RT-thread) */
+  static bool deleteOldCode(World *world, void *rawCallbackData);
+
+  /*! @brief frees the created struct. Uses RTFree since the callback data has
+   *  been allocated within RT thread
+   */
+  static void pluginCmdCallbackCleanup(World *world, void *rawCallbackData);
+
+  /*! @brief a helper method to consume a completion message from the message
+   *  stack makes completionMsg either a nullptr (no message) or point
+   *  it to the buffer within the osc message.
+   */
+  static std::pair<int, const char *> getCompletionMsg(sc_msg_iter *args);
+};
