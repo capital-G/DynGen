f67ffabd7f661d2d48e2cc9940fa6b659701913d
Christof Ressi <christof.ressi@gmx.at>
2026-02-06 03:06:35 +0100

    in/out: do not clamp out-of-bound channels, instead return 0.0 resp. ignore

--- src/eel2_adapter.h
+++ src/eel2_adapter.h
@@ -35,8 +35,8 @@ public:
     static EEL_F eelBufReadC(void* opaque, INT_PTR numParams, EEL_F** params);
     static EEL_F eelBufWrite(void* opaque, INT_PTR numParams, EEL_F** param);
 
-    static EEL_F* in(void* opaque, EEL_F* channel);
-    static EEL_F* out(void* opaque, EEL_F* channel);
+    static EEL_F* eelIn(void* opaque, EEL_F* channel);
+    static EEL_F* eelOut(void* opaque, EEL_F* channel);
 
     static EEL_F eelClip(void*, INT_PTR numParams, EEL_F** params);
     static EEL_F eelWrap(void*, INT_PTR numParams, EEL_F** param);
